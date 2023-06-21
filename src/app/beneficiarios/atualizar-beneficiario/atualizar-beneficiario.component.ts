42b026f0de0964db4ceb47fa16b387edbde535f5
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2023-06-21 08:17:27 -0300

    Adjust atualizar-beneficiario to update documentos

--- src/app/beneficiarios/atualizar-beneficiario/atualizar-beneficiario.component.ts
+++ src/app/beneficiarios/atualizar-beneficiario/atualizar-beneficiario.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { BeneficiariosService } from '../beneficiarios.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Beneficiario } from '../beneficiario.interface';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
+import { BeneficiarioComDocumentos } from '../beneficiario-com-documentos.interface';
 
 @Component({
   selector: 'app-atualizar-beneficiario',
@@ -12,13 +13,14 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AtualizarBeneficiarioComponent {
 
-  beneficiario: Beneficiario | undefined;
+  beneficiario: BeneficiarioComDocumentos | undefined;
   errors: string[] = [];
   form: FormGroup = this.formBuilder.group({
     id: ['', Validators.required],
     nome: ['', Validators.required],
     telefone: ['', Validators.required],
     dataNascimento: ['', Validators.required],
+    documentosDTOs: this.formBuilder.array([])
   });
 
   constructor(
@@ -37,22 +39,56 @@ export class AtualizarBeneficiarioComponent {
     this.service.get(id).subscribe(data => this.updateBeneficiario(data));
   }
 
-  updateBeneficiario(data: Beneficiario) {
+  updateBeneficiario(data: BeneficiarioComDocumentos) {
     this.beneficiario = data;
     this.form = this.formBuilder.group({
       id: [this.beneficiario.id, Validators.required],
       nome: [this.beneficiario.nome, Validators.required],
       telefone: [this.beneficiario.telefone, Validators.required],
       dataNascimento: [this.beneficiario.dataNascimento + 'T00:00:00', Validators.required],
+      documentosDTOs: this.formBuilder.array([])
+    });
+    this.beneficiario.documentos.forEach(documento => {
+      const documentoForm = this.formBuilder.group({
+        id: [documento.id],
+        tipoDocumento: [documento.tipoDocumento, Validators.required],
+        descricao: [documento.descricao, Validators.required]
+      });
+  
+      this.documentos.push(documentoForm);
     });
   }
 
   onSubmit() {
-    const beneficiarioAtualizado: Beneficiario = this.form.value;
+    const beneficiarioAtualizado: BeneficiarioComDocumentos = this.form.value;
+    console.log(beneficiarioAtualizado);
+    console.log(JSON.stringify(beneficiarioAtualizado));
     this.service.update(beneficiarioAtualizado).subscribe({
       next: () => this.router.navigate(['/']),
       error: err => this.handleError(err)
     });
+  }
+
+  get documentos(): FormArray {
+    return this.form.controls['documentosDTOs'] as FormArray;
+  }
+
+  get documentosFormArrayControls(): FormGroup[] {
+    return this.documentos.controls as FormGroup[];
+  }
+
+  addDocumento() {
+    const documentoForm = this.formBuilder.group({
+      id: [''],
+      tipoDocumento: ['', Validators.required],
+      descricao: ['', Validators.required]
+    });
+
+    this.documentos.push(documentoForm);
+  }
+
+  deleteDocumento(index: number) {
+    this.documentos.removeAt(index);
   }
 
   private handleError(err: HttpErrorResponse) {
